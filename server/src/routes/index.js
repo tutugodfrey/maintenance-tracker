87160eae3dc8d22f21f4ce5ffbf38b0716947ac5
tutu godfrey <godfrey_tutu@yahoo.com>
2018-05-15 13:18:39 +0100

    [#157581203] import requestController to route/index and define route to create/add requests

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,16 @@
-
+import RequestController from './../controllers/RequestController';
+const requestController = new RequestController();
 const Routes = class {
+  constructor() {
+    this.requestController = requestController;
+  }
   /* eslint-disable class-methods-use-this */
   routes(app) {
     app.get('/', (req, res) => {
       res.status(200).send({ message: 'welcome to the maintenance trackers' });
     });
+
+    app.post('/api/v1/users/requests', this.requestController.addRequest);
   }
 };
 

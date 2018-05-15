8b8f52aac2ae7cb66706dae56c1e496e2cc539a7
tutu godfrey <godfrey_tutu@yahoo.com>
2018-05-15 10:31:43 +0100

    include routes/index.js with / route and import route file to app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import Routes from './routes/index';
 
+const route = new Routes();
 // setup server class
 class Server {
   constructor() {
     this.express = express;
     this.bodyParser = bodyParser;
     this.logger = morgan;
+    this.route = route;
   }
   expressServer() {
     this.app = this.express();
     this.app.use(this.express.static('public'));
     this.app.use(this.bodyParser.urlencoded({ extended: true }));
     this.app.use(this.bodyParser.json());
+    this.route.routes(this.app);
     return this.app;
   }
 }

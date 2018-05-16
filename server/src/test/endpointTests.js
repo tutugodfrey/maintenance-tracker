e5d4a63de368fc1a6b5f0aede440269fcdba6466
tutu godfrey <godfrey_tutu@yahoo.com>
2018-05-16 21:18:20 +0100

    [#157581260] write passing tests for post ../users/requests endpoint

--- server/src/test/endpointTests.js
+++ server/src/test/endpointTests.js
@@ -7,6 +7,42 @@ const app = server.expressServer();
 const { expect } = chai;
 chai.use(chaiHttp);
 
+const request1 = {
+  category: 'electrical',
+  description: 'Socket  burned',
+  urgency: 'urgent',
+  department: 'baking',
+  userId: 1,
+  status: 'pending',
+};
+const request2 = {
+  category: 'electrical',
+  description: 'Socket  burned',
+  urgency: 'urgent',
+  department: 'baking',
+  userId: 2,
+  status: 'pending',
+};
+
+const request3 = {
+  category: 'electrical',
+  description: 'Socket  burned',
+  urgency: 'urgent',
+  department: 'baking',
+  userId: 4,
+  status: 'pending',
+};
+
+const request4 = {
+  category: 'electrical',
+  description: '',
+  urgency: 'urgent',
+  department: 'baking',
+  userId: 2,
+  status: 'pending',
+};
+const createdRequest1 = {};
+const createdRequest2 = {};
 // enforce test to run in test env
 if (process.env.NODE_ENV !== 'test') {
   /* eslint-disable no-console */
@@ -43,6 +79,57 @@ if (process.env.NODE_ENV !== 'test') {
             expect(res).to.have.status(404);
             expect(res.body).to.be.an('object');
             expect(res.body).to.eql({ Error: 'request not found' });
+          });
+      });
+    });
+
+    // test for post ../users/requests
+    describe('Create requests', () => {
+      it('should create a new request', () => {
+        return chai.request(app)
+          .post('/api/v1/users/requests')
+          .send(request1)
+          .then((res) => {
+            Object.assign(createdRequest1, res.body.request);
+            expect(res).to.have.status(201);
+            expect(res.body).to.be.an('object');
+            expect(res.body.request.id).to.equal(1);
+            expect(res.body.request).to.have.any.keys(['description', 'category', 'userId']);
+          });
+      });
+
+      it('should create a new request', () => {
+        return chai.request(app)
+          .post('/api/v1/users/requests')
+          .send(request2)
+          .then((res) => {
+            Object.assign(createdRequest2, res.body.request);
+            expect(res).to.have.status(201);
+            expect(res.body).to.be.an('object');
+            expect(res.body.request.id).to.equal(2);
+            expect(res.body.request).to.have.any.keys('description');
+            expect(res.body.request).to.have.any.keys('category');
+            expect(res.body.request).to.have.any.keys('userId');
+          });
+      });
+      it('should not create request for users that does not exist', () => {
+        return chai.request(app)
+          .post('/api/v1/users/requests')
+          .send(request3)
+          .then((res) => {
+            expect(res).to.have.status(404);
+            expect(res.body).to.be.an('object');
+          });
+      });
+
+      it('should not create request if a required field is not present', () => {
+        return chai.request(app)
+          .post('/api/v1/users/requests')
+          .send(request4)
+          .then((res) => {
+            expect(res).to.have.status(400);
+            expect(res.body).to.be.an('object');
+            expect(res.body).to.eql({ message: 'missing required field' });
           });
       });
     });
--- server/trans/test/endpointTests.js
+++ server/trans/test/endpointTests.js
@@ -20,6 +20,42 @@ var expect = _chai2.default.expect;
 
 _chai2.default.use(_chaiHttp2.default);
 
+var request1 = {
+  category: 'electrical',
+  description: 'Socket  burned',
+  urgency: 'urgent',
+  department: 'baking',
+  userId: 1,
+  status: 'pending'
+};
+var request2 = {
+  category: 'electrical',
+  description: 'Socket  burned',
+  urgency: 'urgent',
+  department: 'baking',
+  userId: 2,
+  status: 'pending'
+};
+
+var request3 = {
+  category: 'electrical',
+  description: 'Socket  burned',
+  urgency: 'urgent',
+  department: 'baking',
+  userId: 4,
+  status: 'pending'
+};
+
+var request4 = {
+  category: 'electrical',
+  description: '',
+  urgency: 'urgent',
+  department: 'baking',
+  userId: 2,
+  status: 'pending'
+};
+var createdRequest1 = {};
+var createdRequest2 = {};
 // enforce test to run in test env
 if (process.env.NODE_ENV !== 'test') {
   /* eslint-disable no-console */
@@ -49,7 +85,45 @@ if (process.env.NODE_ENV !== 'test') {
         return _chai2.default.request(app).get('/api/v1/users/requests/1').then(function (res) {
           expect(res).to.have.status(404);
           expect(res.body).to.be.an('object');
-          expect(res.body).to.eql({ 'Error': 'request not found' });
+          expect(res.body).to.eql({ Error: 'request not found' });
+        });
+      });
+    });
+
+    describe('Create requests', function () {
+      it('should create a new request', function () {
+        return _chai2.default.request(app).post('/api/v1/users/requests').send(request1).then(function (res) {
+          Object.assign(createdRequest1, res.body.request);
+          expect(res).to.have.status(201);
+          expect(res.body).to.be.an('object');
+          expect(res.body.request.id).to.equal(1);
+          expect(res.body.request).to.have.any.keys(['description', 'category', 'userId']);
+        });
+      });
+
+      it('should create a new request', function () {
+        return _chai2.default.request(app).post('/api/v1/users/requests').send(request2).then(function (res) {
+          Object.assign(createdRequest2, res.body.request);
+          expect(res).to.have.status(201);
+          expect(res.body).to.be.an('object');
+          expect(res.body.request.id).to.equal(2);
+          expect(res.body.request).to.have.any.keys('description');
+          expect(res.body.request).to.have.any.keys('category');
+          expect(res.body.request).to.have.any.keys('userId');
+        });
+      });
+      it('should not create request for users that does not exist', function () {
+        return _chai2.default.request(app).post('/api/v1/users/requests').send(request3).then(function (res) {
+          expect(res).to.have.status(404);
+          expect(res.body).to.be.an('object');
+        });
+      });
+
+      it('should not create request if a required field is not present', function () {
+        return _chai2.default.request(app).post('/api/v1/users/requests').send(request4).then(function (res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.eql({ message: 'missing required field' });
         });
       });
     });

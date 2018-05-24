dc9a4c6b0fb90a7794bb9304f023dcae3ba43946
tutu godfrey <godfrey_tutu@yahoo.com>
2018-05-24 06:28:14 +0100

    chore(test-admin-endpoints): admin is able to modify the status of a request
    - ensure isAdmin is passed in query and set to true
    - ensure requestId is supplied in params
    
    [Delivers #157829827]

--- server/src/controllers/AdminController.js
+++ server/src/controllers/AdminController.js
@@ -45,7 +45,7 @@ const AdminController = class {
     if (isAdmin !== 'true') {
       return res.status(402).send({ message: 'you are not permitted to perform this action' });
     }
-    if (isAdmin !== 'true' || !requestId) {
+    if (!requestId) {
       return res.status(400).send({ message: 'missiging required field' });
     }
     return requests
--- server/trans/test/adminTest.js
+++ server/trans/test/adminTest.js
@@ -18,6 +18,8 @@ var _app2 = _interopRequireDefault(_app);
 
 var _signupTest = require('./signupTest');
 
+var _userRequestTest = require('./userRequestTest');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var server = new _app2.default();
@@ -53,6 +55,101 @@ exports.default = describe('Admin Test', function () {
     it('should return all request', function () {
       return _chai2.default.request(app).get('/secure/api/v1/requests?isAdmin=false').set('token', signedInUser.token).then(function (res) {
         expect(res).to.have.status(402);
+      });
+    });
+  });
+
+  describe('Update a request', function () {
+    describe('approve request', function () {
+      it('should return all request', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/' + _userRequestTest.createdRequest1.id + '/approve?isAdmin=true').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.status).to.equal('pending');
+        });
+      });
+
+      it('should return all request', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/' + _userRequestTest.createdRequest1.id + '/approve?isAdmin=false').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(402);
+          expect(res.body).to.be.an('object');
+        });
+      });
+
+      it('should not update a request that does not exist', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/10/approve?isAdmin=true').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(404);
+          expect(res.body).to.be.an('object');
+        });
+      });
+
+      it('should return bad request if requestId is not set', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/0/approve?isAdmin=true').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+        });
+      });
+    });
+
+    describe('reject request', function () {
+      it('should return all request', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/' + _userRequestTest.createdRequest1.id + '/disapprove?isAdmin=true').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.status).to.equal('rejected');
+        });
+      });
+
+      it('should return all request', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/' + _userRequestTest.createdRequest1.id + '/disapprove?isAdmin=false').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(402);
+          expect(res.body).to.be.an('object');
+        });
+      });
+
+      it('should not update a request that does not exist', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/10/disapprove?isAdmin=true').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(404);
+          expect(res.body).to.be.an('object');
+        });
+      });
+
+      it('should return bad request if requestId is not set', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/0/disapprove?isAdmin=true').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+        });
+      });
+    });
+
+    describe('resolve request', function () {
+      it('should return all request', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/' + _userRequestTest.createdRequest1.id + '/resolve?isAdmin=true').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.status).to.equal('resolved');
+        });
+      });
+
+      it('should return all request', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/' + _userRequestTest.createdRequest1.id + '/resolve?isAdmin=false').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(402);
+          expect(res.body).to.be.an('object');
+        });
+      });
+
+      it('should not update a request that does not exist', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/10/resolve?isAdmin=true').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(404);
+          expect(res.body).to.be.an('object');
+        });
+      });
+
+      it('should return bad request if requestId is not set', function () {
+        return _chai2.default.request(app).put('/secure/api/v1/requests/0/resolve?isAdmin=true').set('token', signedInUser.token).then(function (res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+        });
       });
     });
   });

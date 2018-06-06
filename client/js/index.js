1ea74d0435a12095d8bf9eb48239d1be39d74a6e
tutu godfrey <godfrey_tutu@yahoo.com>
2018-06-06 21:39:57 +0100

    feat(user signin client): add functionality for user to signin
    - ensure user signin with valid cridential
    - ensure user is redirected to their dashboard upon successful signin
    - ensure user receive console message if signin fails
    
    [Delivers #157970598]

--- client/js/index.js
+++ client/js/index.js
@@ -1,164 +1,63 @@
-let defaultNavItem;
-// domNotifier();
-  const newEvent = function (elementObject,  eventType, callBack, callBackArgument) {
-     elementObject.addEventListener(eventType, function(event) {
-     event.preventDefault(); 
-       if(callBackArgument === undefined) {
-         callBack();
-       } else {
-         callBack(callBackArgument);
-       }
-     },
-     false );
-  }   // end newEvent
+const domElements = new DomElementActions();
+const storageHandler = new StorageHandler();
+const requestHandler = new RequestHandler();
 
-  const getUserData = function (key) {
-    if (localStorage.getItem(key)) {
-      let userInfo = localStorage.getItem(key);
-      userInfo = JSON.parse(userInfo);
-      return userInfo;
-    } else {
-      return 'no user data stored in local storage';
-    }
+// process server response
+const handleResponse = (responseData) => {
+  if (responseData.message) {
+    domElements.showConsoleModal(responseData.message);
+    return;
+  } else {
+    storageHandler.storeData(responseData, 'userdata');
+    storageHandler.redirectUser(responseData);
   }
-
-  const changeAttribute  =function (eleObject, attrToChange, newAttr, removeAttr) {
-    const eleAttr = eleObject.getAttribute(attrToChange);
-    if (eleAttr === null || eleAttr !== newAttr) 	{
-      //set the attribute and the image become large
-      eleObject.setAttribute(attrToChange, newAttr);
-    } else if (eleAttr === newAttr && removeAttr === 'yes') {
-      eleObject.removeAttribute(attrToChange);
-    }  else if (eleAttr === newAttr && removeAttr === 'no') {
-      //no action taken
-    } else if(eleAttr === newAttr && (removeAttr !== 'yes' || removeAttr !== 'no' || removeAttr !== "undefined")){
-      eleObject.setAttribute(attrToChange, removeAttr);
-    } 
-  }		//end changeClass
-
-
-  const showNavigation =  function(toggleBtn) {
-    const verticalNav = document.getElementById('vertical-nav-bar');
-    const classValue = verticalNav.getAttribute('class');
-    if(classValue === 'hide-item') {
-      changeAttribute(verticalNav, 'class', 'show-item')
-    } else if(classValue === 'show-item') {
-      changeAttribute(verticalNav, 'class', 'hide-item')
-    }
-  }
-  // get the id of the default tab view
-  let lastElementClicked;
-  let defaultTab;
-  function tabNavigation(elem) {
-    //change the class for this element
-    const elemClass = elem.getAttribute('class');
-    const classArr = elemClass.split(' ');
-    if(classArr.indexOf('inactive') >= 0) {
-      // deactive old active nav
-      const activeNav = document.getElementsByClassName('active');
-      for(let activeNavItem of activeNav) {
-        let activeNavClass = activeNavItem['className'].replace('active', 'inactive');
-        activeNavItem.setAttribute('class', activeNavClass);
+}
+const processSignin = (ele) => {
+  if (document.getElementsByClassName('form-control')) {
+    const formControls = document.getElementsByClassName('form-control');
+    let requestData = '';
+    for(let numOfInput = 0; numOfInput < formControls.length; numOfInput++) {
+      let inputField = formControls[numOfInput];
+      const fieldName = inputField.name;
+      const fieldValue = inputField.value.trim();
+      const eleClass = inputField.getAttribute('class');
+      if (eleClass.indexOf('required-field') > 0) {
+        if (fieldValue.trim() === '') {
+          domElements.showConsoleModal('Please fill out the the required fields');
+          return;
+        }
+        requestData = `${requestData}${fieldName}=${fieldValue}&`;
+      } else {
+      requestData = `${requestData}${fieldName}=${fieldValue}&`;
       }
-    
-      // activate new active nav
-      const newClassValue = elemClass.replace('inactive', 'active');
-      elem.setAttribute('class', newClassValue)
     }
-
-    // display tab nav item
-    const anchorPos = elem.href.indexOf('#');
-    const href = elem.href.substring(anchorPos + 1);
-    const tabSection = document.getElementById(href);
-    const classValue = tabSection.getAttribute('class');
-    if((!lastElementClicked && href !== defaultNavItem)) {
-      changeAttribute(tabSection, 'class', 'tab-item show-item');
-      if(document.getElementById('view-requests')) {
-        defaultTab = document.getElementById('view-requests');
-        changeAttribut(defaultTab, 'class', 'tab-item hide-item');
-      }
-
-      if(document.getElementById('request-repair')) {
-        defaultTab = document.getElementById('request-repair');
-        changeAttribute(defaultTab, 'class', 'tab-item hide-item');
-      }
-
-    } else if(lastElementClicked && href !== lastElementClicked) {
-      if(classValue === 'tab-item hide-item') {
-        changeAttribute(tabSection, 'class', 'tab-item show-item');
-      } 
-      const oldTabSection = document.getElementById(lastElementClicked);
-      changeAttribute(oldTabSection, 'class', 'tab-item hide-item');
-    } else {
-      if(classValue === 'tab-item show-item') {
-        changeAttribute(tabSection, 'class', 'tab-item show-item');
-      }
-
+    const headers =  new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers.append('Accept', 'application/json');
+    const options = {
+      headers,
+      method: 'POST',
+      body:requestData,
     }
-
-    lastElementClicked = href;
+    requestHandler.makeRequest('/api/v1/auth/signin', options, handleResponse)
   }
+} // end processSignin
 
-  const showConsoleModal = function(message) {
-    if(document.getElementById('console-modal')) {
-      const consoleModal = document.getElementById('console-modal');
-      const messageBox = document.getElementById('message-box');
-      messageBox.innerHTML = message;
-      let consoleModalClass = consoleModal.getAttribute('class');
-      consoleModalClass = consoleModalClass.replace('hide-item', 'show-item');
-      consoleModal.setAttribute('class', consoleModalClass);
-    }
-  }
-  
-  const closeConsoleModal = function(message) {
-    if(document.getElementById('console-modal')) {
-      const consoleModal = document.getElementById('console-modal');
-      let consoleModalClass = consoleModal.getAttribute('class');
-      consoleModalClass = consoleModalClass.replace('show-item', 'hide-item');
-      consoleModal.setAttribute('class', consoleModalClass);
-    }
-  }
-
-
-
-function domNotifier() {
+const domNotifier = () => { 
  // localStorage.removeItem('userdata')
-  if(document.getElementById('toggle-navigation-btn')) {
-    const toggleNavBtn = document.getElementById('toggle-navigation-btn');
-      newEvent(toggleNavBtn, 'click', showNavigation, toggleNavBtn);
-  }
-
-  if(document.getElementsByClassName('nav-link')) {
-    const navItems = document.getElementsByClassName('nav-link');
-    for(let size = 0; size < navItems.length; size++) {
-      newEvent(navItems[size], 'click', tabNavigation, navItems[size]);
-    }  
-  }
-
-  if(document.getElementById('default-nav')) {
-    // get the default nav item
-    const defaultNav = document.getElementById('default-nav');
-    const anchorPos = defaultNav.href.indexOf('#');
-    defaultNavItem = defaultNav.href.substring(anchorPos + 1);
-  }
-
   // console modal
+  const userData = storageHandler.getDataFromStore('userdata');
+  storageHandler.redirectUser(userData);
+  storageHandler.redirectUser(userData);
   if(document.getElementById('console-modal-button')) {
     const consoleModalBtn = document.getElementById('console-modal-button');
-    newEvent(consoleModalBtn, 'click', closeConsoleBox, consoleModalBtn);
+    domElements.newEvent(consoleModalBtn, 'click', domElements.closeConsoleModal,  domElements);
   }
 
-  if(document.getElementById('signup-button')) {
-		const signupButton = document.getElementById('signup-button');
-		newEvent(signupButton, 'click', processSignUp, signupButton);
+  if(document.getElementById('inline-signin-button')) {
+		const signinButton = document.getElementById('inline-signin-button');
+		domElements.newEvent(signinButton, 'click', processSignin, signinButton);
 	}
-  const userData = getUserData('userdata');
- if(userData.isAdmin === true  || userData.isAdmin === 'on') {
-		window.location.href= '/admin/dashboard.html';
-	} else if (userData.isAdmin === false){
-		window.location.href = '/users/dashboard.html';
-  } 
-  
 }
 
 domNotifier();
---
+++ client/js/utilities.js
@@ -0,0 +1,183 @@
+
+const DomElementActions = class {
+  constructor() {
+    this.defaultTab;
+    this.lastElementClicked;
+    this.defaultNavItem;
+    this.changeClassValue = this.changeClassValue.bind(this);
+    // this._changeClassValue = this._changeClassValue;
+  }
+  // add eventlistener to elements
+  newEvent (elementObject,  eventType, callBack, callBackArgument) {
+    elementObject.addEventListener(eventType, function(event) {
+    event.preventDefault(); 
+      if(callBackArgument === undefined) {
+        callBack();
+      } else {
+        callBack(callBackArgument);
+      }
+    },
+    false );
+  }   // end newEvent
+
+  // change the attribute of elements
+  changeAttribute (eleObject, attrToChange, newAttr, removeAttr) {
+    const eleAttr = eleObject.getAttribute(attrToChange);
+    if (eleAttr === null || eleAttr !== newAttr) 	{
+      //set the attribute and the image become large
+      eleObject.setAttribute(attrToChange, newAttr);
+    } else if (eleAttr === newAttr && removeAttr === 'yes') {
+      eleObject.removeAttribute(attrToChange);
+    }  else if (eleAttr === newAttr && removeAttr === 'no') {
+      //no action taken
+    } else if(eleAttr === newAttr && (removeAttr !== 'yes' || removeAttr !== 'no' || removeAttr !== "undefined")){
+      eleObject.setAttribute(attrToChange, removeAttr);
+    } 
+  }		//end changeClass
+
+  // change the attribute of elements
+  changeClassValue (eleObject, valueToChange, replacement) {
+    let eleClassValue = eleObject.getAttribute('class');
+    eleClassValue = eleClassValue.replace(valueToChange, replacement);
+    eleObject.setAttribute('class', eleClassValue);
+  }		//end changeClassValue
+
+  // display and close vertical nav button
+  showNavigation(toggleBtn) {
+    const verticalNav = document.getElementById('vertical-nav-bar');
+    const classValue = verticalNav.getAttribute('class');
+    if(classValue.indexOf('hide-item') >= 0) {
+      this.changeClassValue (verticalNav, 'hide-item', 'show-item');
+    } else if(classValue.indexOf('show-item') >= 0) {
+      this.changeClassValue (verticalNav, 'show-item', 'hide-item');
+    }
+  }
+
+  tabNavigation(elem) {
+    // deactive current activeNav
+    const activeNav = document.getElementsByClassName('active')[0];
+    this.changeClassValue (activeNav, 'active', 'inactive');
+
+    // activate elem 
+    this.changeClassValue (elem, 'inactive', 'active');
+ 
+    // display tab elements
+    const anchorPos = elem.href.indexOf('#');
+    // value in href = id of the tab section
+    const href = elem.href.substring(anchorPos + 1);
+    const tabSection = document.getElementById(href);
+    // const classValue = tabSection.getAttribute('class');
+    if((!this.lastElementClicked && href !== defaultNavItem)) {
+      this.changeClassValue (tabSection, 'hide-item', 'show-item');
+      if(document.getElementById('view-requests')) {
+        defaultTab = document.getElementById('view-requests');
+        this.changeClassValue (defaultTab, 'show-item', 'hide-item')
+      }
+      if(document.getElementById('request-repair')) {
+        defaultTab = document.getElementById('request-repair');
+        this.changeClassValue (defaultTab, 'show-item', 'hide-item')
+      }
+    } else if(this.lastElementClicked && href !== this.lastElementClicked) {
+      this.changeClassValue (tabSection, 'hide-item', 'show-item');
+      const oldTabSection = document.getElementById(this.lastElementClicked);
+      this.changeClassValue (oldTabSection, 'show-item', 'hide-item');
+    } else {
+      this.changeClassValue (tabSection, 'hide-item', 'show-item');
+    }
+     this.lastElementClicked = href;
+   }
+
+  showConsoleModal (message) {
+    if(document.getElementById('console-modal')) {
+      const consoleModal = document.getElementById('console-modal');
+      const messageBox = document.getElementById('message-box');
+      messageBox.innerHTML = message;
+      this.changeClassValue(consoleModal, 'hide-item', 'show-item');
+    }
+  }
+  
+  closeConsoleModal (self) {
+    // self = the instance of this class
+    if(document.getElementById('console-modal')) {
+      const consoleModal = document.getElementById('console-modal');
+      self.changeClassValue(consoleModal, 'show-item', 'hide-item');
+    }
+  }
+}
+
+const StorageHandler = class {
+  getDataFromStore (key) {
+    if (typeof key !== 'string') {
+      return `typeError: expecting a string but got ${typeof key}`;
+    } 
+    if (localStorage.getItem(key)) {
+      let data = localStorage.getItem(key);
+      data = JSON.parse(data);
+      return data;
+    } else {
+      return 'no user data stored in local storage';
+    }
+  }
+  
+  // store data to localStorage
+  storeData(dataToStore, keyString) {
+    if (typeof dataToStore !== 'object') {
+      return `typeError: expecting an object but got ${typeof dataToStore}`;
+    }
+    if (typeof keyString !== 'string') {
+      return `typeError: expecting an string but got ${typeof keyString}`;
+    }
+    if (!localStorage.getItem(keyString)) {
+      const stringifyData = JSON.stringify(dataToStore);
+      localStorage.setItem(keyString, stringifyData);
+      return 'data stored';
+    }
+  }
+  
+  redirectUser(userData) {
+    if (typeof userData !== 'object') {
+      return `typeError: expecting an object but got ${typeof userData}`;
+    }
+    if (userData.isAdmin === true  || userData.isAdmin === 'on') {
+      window.location.href= '/admin/dashboard.html';
+    } else if (userData.isAdmin === false) {
+      window.location.href = '/users/dashboard.html';
+    } 
+  }
+}
+
+const RequestHandler = class {
+  createRequest(ele) {
+    if (document.getElementsByClassName('form-control')) {
+      const formControls = document.getElementsByClassName('request-data');
+      let jsonRequest = '';
+      let fileAvailable = false;
+      for(let numOfInput = 0; numOfInput < formControls.length; numOfInput++) {
+        let inputField = formControls[numOfInput];
+        const fieldName = inputField.name;
+        const fieldValue = inputField.value.trim();
+        const eleClass = inputField.getAttribute('class');
+        if (eleClass.indexOf('required-field') > 0) {
+          if (fieldValue.trim() === '') {
+            showConsoleModal('Please fill out the the required fields');
+            return;
+          }
+          jsonRequest = `${jsonRequest}${fieldName}=${fieldValue}&`;
+        } else {
+        jsonRequest = `${jsonRequest}${fieldName}=${fieldValue}&`;
+        }
+      }
+      jsonRequest = `${jsonRequest}userId=${userData.id}`;
+      makeRequest(jsonRequest, 'POST', '/api/v1/users/requests', handleResponse)
+    }
+  } // end createRequest
+
+  makeRequest(url, options, callback) {
+    fetch(url, options)
+    .then(res => res.json())
+    .then(data => {
+      callback(data)
+    })
+    .catch(error => console.log(error));
+  }
+}

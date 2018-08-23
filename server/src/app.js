8d48a4ed76f8838caee443d6b6d91398c501061c
Godfrey Tutu <godfreytutu@Godfreys-MacBook-Pro.local>
2018-08-23 19:56:18 +0100

    install cors and enable cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import routes from './routes/index';
+
 
 const app = express();
 app.use(express.static('public'));
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 routes(app);
 
 export default app;
